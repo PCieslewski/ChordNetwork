1f5c260c2c8ef61a0ee4fcdcd1464bd2f81ecfa0
PCieslewski <Pawel.vip@gmail.com>
2015-10-21 20:34:50 -0400

    More Stuff

---
+++ src/main/scala-2.11/Finger.scala
@@ -0,0 +1,9 @@
+import akka.actor.ActorRef
+
+class Finger() {
+
+  var start: Long = null
+  var range: Range = null
+  var successor: ActorRef = null
+
+}
--- src/main/scala-2.11/Node.scala
+++ src/main/scala-2.11/Node.scala
@@ -7,11 +7,13 @@ class Node(name: String) {
 
   val id: Long = hash(name)
 
-  var prevNode: ActorRef = null
   var nextNode: ActorRef = null
+
+  var range: Range = null
 
   var dataList: mutable.MutableList[Data] = new mutable.MutableList[Data]
 
+  var fingerTable: Array[Finger] = Array.fill(62)(new Finger())
 
 
 
@@ -21,30 +23,5 @@ class Node(name: String) {
 
 
 
-}
-
-class Range(start: Long, end: Long){
-
-  def inRange(id: Long): Boolean ={
-
-    //Case where the range is not on the boundary of 0 and MAX
-    if(end > start){
-      return inRange(start, end, id)
-    }
-    //Case where range is on the boundary
-    else{
-      return (inRange(start, Long.MaxValue >>> 1, id) | inRange(0, end, id))
-    }
-
-  }
-
-  def inRange(start: Long, end: Long, id: Long): Boolean ={
-    if(id > start && id < end){
-      return true
-    }
-    else{
-      return false
-    }
-  }
 
 }
